05647ea3cfb199d86c6af18389e381cf02085e2c
Andrea Amaranto <amarantoandrea@gmail.com>
2025-10-19 09:41:07 +0200

    Fix CSS import path and SessionProvider Server Component error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import Navbar from "../components/Navbar";
-import "../styles/globals.css"; 
-import { SessionProvider } from "next-auth/react"
+import "@/styles/globals.css";
+import ClientSessionProvider from "../components/ClientSessionProvider";
 
 import { ReactNode } from "react";
 
@@ -8,10 +8,10 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="it">
       <body>
-        <SessionProvider>
+        <ClientSessionProvider>
           <Navbar />
           <main className="container mx-auto px-4 py-8">{children}</main>
-        </SessionProvider>
+        </ClientSessionProvider>
       </body>
     </html>
   );
